45a6cd08bf2ab20c053cb42796c48ca703844bf6
HEMA-HEMDAN <hshaabrahym36@gmail.com>
2026-03-13 08:13:14 +0200

    this is it now it's almost done for me

--- src/modules/auth/auth.routes.js
+++ src/modules/auth/auth.routes.js
@@ -49,7 +49,7 @@ authRouter.get(
     });
 
     const frontendUrl = process.env.FRONTEND_URL || "http://localhost:5173";
-    res.redirect(`${frontendUrl}/auth/callback`);
+    res.redirect(`${frontendUrl}/`);
   },
 );
 
